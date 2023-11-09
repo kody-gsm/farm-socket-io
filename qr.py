9a00ffbce54e270e4e2638e1abb869d2d27f75dd
kimgh06 <kkh061101@naver.com>
2023-11-09 20:39:49 +0900

    성공적으로 네트워크 추가 완료

--- qr.py
+++ qr.py
@@ -21,7 +21,35 @@ r.pop()
 for i in r:
     i = i.split(":")
     wifi_data[i[-2]] = i[-1]
-print(wifi_data['P'], wifi_data['S'], wifi_data['T'])
-capture.release()
+
+psk = ''
+ssid = ''
+key_mgmt = ''
+
+if 'P' in wifi_data.keys():
+    psk = '\n\tpsk="'+wifi_data['P']+'"'
+ssid = '\n\tssid="'+wifi_data['S']+'"'
+if not 'NONE' in wifi_data['T']:
+    key_mgmt = '\n\tkey_mgmt='+wifi_data['T']+'-PSK'
+
+Network_Obj = '{'+ssid+psk+key_mgmt+'\n}\n\n'
+print(Network_Obj)
+
+exists = False
+
+with open("/etc/wpa_supplicant/wpa_supplicant.conf", 'r') as wpa_supplicant:
+    datas = wpa_supplicant.read().split('network=')
+    datas.pop(0)
+    for network in datas:
+        if(network == Network_Obj):
+            exists = True
+  
+if not exists:
+    with open("/etc/wpa_supplicant/wpa_supplicant.conf", 'a') as wpa_supplicant:
+        wpa_supplicant.writelines(['\nnetwork=',Network_Obj])
+        wpa_supplicant.close()
+        print('성공적으로 네트워크를 등록했습니다')
+else:
+    print("이미 존재하는 네트워크 입니다.")
 
 cv2.destroyAllWindows()
