075af69bc9ea2e02f5b433937cad049f14952bea
kimgh06 <kkh061101@naver.com>
2023-11-21 20:25:26 +0900

    백업

---
+++ led.py
@@ -0,0 +1,10 @@
+import board
+import digitalio
+
+# Initial the dht device, with data pin connected to:
+
+d10=board.D10
+led = digitalio.DigitalInOut(d10)
+led.direction = digitalio.Direction.OUTPUT
+
+led.value = True
