5231c22e81e2598f151cfaa299a35db546400138
kimgh06 <kkh061101@naver.com>
2023-11-22 20:15:57 +0900

    dht insam 연결

--- Insam.py
+++ Insam.py
@@ -1,18 +1,36 @@
 import qr
+from dht import Dht
 import os
 import drivers
+import time
+import asyncio
 
 # sudo python3 Insam.py
-display=drivers.Lcd()
-hostname="8.8.8.8"
-response= os.system("ping -c 1 "+hostname)
+async def main():
+    display=drivers.Lcd()
+    hostname="8.8.8.8"
+    response= os.system("ping -c 1 "+hostname)
 
-display.lcd_clear()
-display.lcd_display_extended_string("Checking Network",1)
-if __name__== '__main__':
+    display.lcd_clear()
+    display.lcd_display_extended_string("Checking Network",1)
     if response== 0:
         display.lcd_clear()
         display.lcd_display_extended_string("Network is",1)
         display.lcd_display_extended_string("Enabled",2)
+        while True:
+            dht_task = asyncio.create_task(Dht()) #맨 마지막
+            dht_task = asyncio.create_task(Dht())
+            temp, hum=await dht_task
+            if temp and hum:
+                print(temp,hum)
+                display.lcd_clear()
+                display.lcd_display_extended_string("temp: {}C".format(temp),1)
+                display.lcd_display_extended_string("hum: {}%".format(hum),2)
+                time.sleep(0.5)
+            else:
+                time.sleep(2.0)
     else:
         qr.ResgistNetwork()
+
+if __name__== '__main__':
+    asyncio.run(main())
--- dht.py
+++ dht.py
@@ -12,24 +12,16 @@ dhtDevice = adafruit_dht.DHT11(board.D4)
 # but it will not work in CircuitPython.
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-while True:
+async def Dht():
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        print(
-            "Temp:  {:.1f} C    Humidity: {}% ".format(
-                temperature_c, humidity
-            )
-        )
+        return temperature_c, humidity
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        time.sleep(2.0)
-        continue
     except Exception as error:
         dhtDevice.exit()
         raise error
-
-    time.sleep(1.0)
