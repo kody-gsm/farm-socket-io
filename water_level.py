5acca29d63c557d7d5dc0387d0a45ea9921948d3
kimgh06 <kkh061101@naver.com>
2024-01-14 13:51:21 +0900

    water level

---
+++ water_level.py
@@ -0,0 +1,39 @@
+import spidev
+import time
+import math
+
+delay = 0.1
+
+# Open Spi Bus
+# SPI 버스 0과 디바이스 0을 열고
+# 최대 전송 속도를 1MHz로 설정
+spi = spidev.SpiDev()
+spi.open(1,0) # open(bus, device)
+spi.max_speed_hz = 1000000 # set transfer speed
+spi.mode = 0
+
+# To read SPI data from MCP3008 chip
+# Channel must be 0~7 integer
+def readChannel(channel): 
+  val = spi.xfer2([1, (8+channel)<<4, 0])
+  data = ((val[1]&3) << 8) + val[2]
+  return data
+
+# 0~1023 value가 들어옴. 1023이 수분함량 min값
+def convertPercent(data):
+  return (100.0-round(((data*100)/float(1023)),1))*2
+
+def soil_hemi():
+  try:
+    val = readChannel(0)
+    if (val != 0) : # filtering for meaningless num
+      print(val, "/", math.ceil(convertPercent(val)*100)/100,"%")
+    else:
+      print('err')
+    time.sleep(delay)
+  except KeyboardInterrupt:
+    spi.close()
+    print("Keyboard Interrupt!!!!")
+
+while True:
+  soil_hemi()
