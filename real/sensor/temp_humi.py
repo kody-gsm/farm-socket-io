37fbd11250d3cbdfd1d90181eaf4faa7bbd10ab3
byundojin <103907292+byundojin@users.noreply.github.com>
2024-02-01 14:57:49 +0900

    rtsyuio

--- real/sensor/temp_humi.py
+++ real/sensor/temp_humi.py
@@ -14,15 +14,13 @@ class TempHumiSensor(Sensor, object):
     def __init__(self) -> None:
         self.dhtDevice:adafruit_dht.DHT11
 
-    def setting(self, func):
+    def __enter__(self):
         self.dhtDevice = adafruit_dht.DHT11(board.D4)
+        return self
 
-        result = func(self)
-
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.dhtDevice.exit()
-        return result
 
-    @setting()
     def get_data(self):
         try:
         # Print the values to the serial port
@@ -32,5 +30,5 @@ class TempHumiSensor(Sensor, object):
         except RuntimeError as error:
             return "err"
         
-s1 = TempHumiSensor()
-print(s1.get_data())
+with TempHumiSensor() as s1:
+    print(s1.get_data())
