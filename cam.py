c83c60237d0767f75ddc8d3408714866bc616d29
kimgh06 <kkh061101@naver.com>
2024-02-27 14:51:42 +0900

    like non-stop

--- cam.py
+++ cam.py
@@ -7,7 +7,7 @@ from websockets.exceptions import ConnectionClosedError
 
 USER_NAME = 'rpi_1234'
 
-BACKEND_URL = '192.168.1.4:8000'
+BACKEND_URL = '116.124.89.131:8000'
 
 async def capture(ws):
     if not cv2.waitKey(33) < 0:
@@ -25,26 +25,25 @@ async def capture(ws):
         _, img_en = cv2.imencode('.jpg', frame)
         img_64 =base64.b64encode(img_en)
         img_str = img_64.decode('utf-8')
-        print(img_str[:20])
         message ={'type':'image','message':img_str}
         await ws.send(json.dumps(message))
     except Exception as e:
         print(e)
+        return False
     cam.release()
+    return True
 
 async def main():
     url = f"ws://{BACKEND_URL}/ws/{USER_NAME}"
     print(url)
     try:
-        for ii in range(200):
+        for _ in range(200):
             async with websockets.connect(url, ping_interval=None) as websocket:
-                for i in range(1):
-                    print(i)
-                    try:
-                        await asyncio.gather(capture(websocket))
-                    except websockets.ConnectionClosed:
-                        print('tttterminate')
-                await websocket.close()
+                while True:
+                    result = await asyncio.gather(capture(websocket))
+                    print(result[0])
+                    if result[0] == False:
+                        break
 
     except ConnectionClosedError as e:
         print(f"WebSocket connection closed unexpectedly: {e}")
