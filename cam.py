6b8881ff7179d6f5bd83ea51ab4d3952130b44dd
kimgh06 <kkh061101@naver.com>
2024-03-21 20:26:51 +0900

    edit capture function at cam.py to indepentant function

--- cam.py
+++ cam.py
@@ -9,14 +9,17 @@ USER_NAME = 'rpi_1234'
 
 BACKEND_URL = '116.124.89.131:8000'
 
-async def capture(ws):
-    if not cv2.waitKey(33) < 0:
-        return
-    cam = cv2.VideoCapture(0)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
+async def capture():
     try:
+        if not cv2.waitKey(33) < 0:
+            return
+        cam = cv2.VideoCapture(0)
+        # print(cam.isOpened())
+        if not cam.isOpened():
+            raise Exception("can't get camera")
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
         ret,frame  =cam.read()
         if not ret:
             return
@@ -25,25 +28,26 @@ async def capture(ws):
         _, img_en = cv2.imencode('.jpg', frame)
         img_64 =base64.b64encode(img_en)
         img_str = img_64.decode('utf-8')
-        message ={'type':'image','message':img_str}
-        await ws.send(json.dumps(message))
+        message =json.dumps({'type':'image','message':img_str})
     except Exception as e:
-        print(e)
+        # print(e)
         return False
     cam.release()
-    return True
+    return message
 
 async def main():
     url = f"ws://{BACKEND_URL}/ws/{USER_NAME}"
     print(url)
     try:
         for _ in range(200):
-            async with websockets.connect(url, ping_interval=None) as websocket:
+            # async with websockets.connect(url, ping_interval=None) as websocket:
                 while True:
-                    result = await asyncio.gather(capture(websocket))
-                    print(result[0])
+                    result = await asyncio.gather(capture())
                     if result[0] == False:
+                        print("can't get camera")
                         break
+                    print('it works')
+                    # await websocket.send(result)
 
     except ConnectionClosedError as e:
         print(f"WebSocket connection closed unexpectedly: {e}")
