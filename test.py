03a207eb8e25f3b8d1d3e51dd1363f6019ab90b2
byundojin <103907292+byundojin@users.noreply.github.com>
2024-05-08 19:47:23 +0900

    123

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-from sensor.cam import CamSenSor
+# from sensor.cam import CamSenSor
 
-with CamSenSor() as cam:
-    try:
-        data = cam.get_data()
-        while not data:
-            print("cam none")
-            data = cam.get_data()
-        print(len(data))
-    except Exception as e:
-        print(e)
+# with CamSenSor() as cam:
+#     try:
+#         data = cam.get_data()
+#         while not data:
+#             print("cam none")
+#             data = cam.get_data()
+#         print(len(data))
+#     except Exception as e:
+#         print(e)
+
+from sensor.temp_humi import TempHumiSensor
+
+with TempHumiSensor() as s:
+    print(s.get_data())
